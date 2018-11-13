96ad2f3e85eb60d1c801d30d54959787000d7f20
c-forden <44967664+c-forden@users.noreply.github.com>
2018-11-13 11:13:55 +0100

    Add files via upload
    And his name is John Cena!

---
+++ Amazing.ps1
@@ -0,0 +1,16 @@
+﻿Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.Speak('And his name is John Cena.')
+[console]::beep(440,300)
+[console]::beep(500,180)
+Start-Sleep -m 100
+[console]::beep(400,200)
+Start-Sleep -m 100
+[console]::beep(440,750)
+Start-Sleep -m 500
+[console]::beep(530,300)
+[console]::beep(500,180)
+Start-Sleep -m 100
+[console]::beep(400,200)
+Start-Sleep -m 100
+[console]::beep(440,750)
